2c2a0f3defe8b82fff66f32f069b422f3e335f9c
Shane Short <shanes@webinabox.net.au>
2022-03-03 11:09:52 +0800

    Add Dynamic CORS headers based on regex match

--- backend/empty.php
+++ backend/empty.php
@@ -2,10 +2,15 @@
 
 header('HTTP/1.1 200 OK');
 
-if (isset($_GET['cors'])) {
-    header('Access-Control-Allow-Origin: *');
+if (
+	(isset($_GET['cors'])) && 
+ 	(isset($_SERVER['HTTP_ORIGIN'])) &&
+	(preg_match('/http(s?):\/\/speedtest.*nutrinet\.io$/', $_SERVER['HTTP_ORIGIN']))
+   ) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Methods: GET, POST');
     header('Access-Control-Allow-Headers: Content-Encoding, Content-Type');
+    header('Vary: Origin');
 }
 
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0');
