65b2d6576d2376358cb9b5834951fde14068a74f
Shane Short <shanes@webinabox.net.au>
2022-03-03 13:10:27 +0800

    adding CORS to garbage.php

--- backend/garbage.php
+++ backend/garbage.php
@@ -32,9 +32,15 @@ function sendHeaders()
 {
     header('HTTP/1.1 200 OK');
 
-    if (isset($_GET['cors'])) {
-        header('Access-Control-Allow-Origin: *');
+    if (
+        (isset($_GET['cors'])) &&
+        (isset($_SERVER['HTTP_ORIGIN'])) &&
+        (preg_match('/http(s?):\/\/speedtest.*nutrinet\.io$/', $_SERVER['HTTP_ORIGIN']))
+       ) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Methods: GET, POST');
+        header('Access-Control-Allow-Headers: Content-Encoding, Content-Type');
+        header('Vary: Origin');
     }
 
     // Indicate a file download
